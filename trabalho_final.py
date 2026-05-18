7c35277b757db889ac8e46471df3258511b8653d
Luzzzkkkkk <Luznerdola@gmail.com>
2026-05-18 15:13:25 -0300

    Update trabalho_final.py

--- trabalho_final.py
+++ trabalho_final.py
@@ -10,4 +10,21 @@ def conectar():
     )
 
 
+def cadastro():
+    print("=-=-=-=-=-=-=-Cadastro-=-=-=-=-=-=-=-")
+    print("1 - Estudante")
+    print("2 - Professor")
+    while True:
+        try:
+            cargo = input("Digite seu cargo: ")
+            if cargo == "1":
+                input('Digite seu nome: ')
+                input('Digite sua matrícula: ')
+                input('Coloque sua senha: ')
+            elif cargo == "2":
+                input('Digite seu nome: ')
+                input('Digite seu ID: ')
+            else: 
+                print('Erro, tente novamente!')
+
 
