541460b9a7221032bbde346a7869002196e07b49
Luzzzkkkkk <Luznerdola@gmail.com>
2026-05-25 16:32:34 -0300

    def estudante com coisas novas
    Tentei colocar emojis, mas foi mais fácil só copiá-lo e colocar no código. Há muitas coisas a serem feitas a respeito da def estudante, mas houve um avanço (mesmo que pequeno kkkkk).

--- trabalho_final.py
+++ trabalho_final.py
@@ -23,18 +23,29 @@ def cadastro():
                 estudante()
             elif cargo == "2":
                 print("Bem-vindo à área do professor!")
+                prof()
             else: 
                 break
         except ValueError:
             print('Erro, tente novamente!')
                 
 def estudante():
-    
     nome = input('Digite seu nome: ')
+    if any(c.isdigit() for c in nome):
+        print("⚠️   Alerta: O nome contém números. O nome será corrigido.")
     nome_correto = "".join ([c for c in nome if c.isalpha() or c.isspace()])
-    print(nome_correto)
-    matrícula = (input('Digite o número da matrícula: ')).isdigit()
-    senha = input('Coloque sua senha: ').replace(" ","")
+    print(f"Correção do nome: { nome_correto }")
+    while True:
+        matrícula = (input('Digite o número da matrícula: '))
+        if any(c.isalpha() for c in matrícula):
+            print("Apenas números permitidos. Tente novamente.")
+        else:
+            print('Erro, tente novamente!')
+            break
+    
+        senha = input('Coloque sua senha: ').replace(" ","")
 
+def prof():
+    ...
 
 cadastro()
